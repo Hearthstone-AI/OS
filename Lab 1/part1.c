423728a0ce0c3b749772d441bfdae25b46982d32
29418/cs220/27569845 <williamy@elnux1.cs.umass.edu:29418/cs220/27569845>
2016-02-14 21:03:31 -0500

    Part3 finished add whatever you need in the readme

--- Lab 1/part1.c
+++ Lab 1/part1.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>	
 #include <unistd.h>	
 #include <stdio.h>	
-
+#include <sys/wait.h>
 
 int main(int argc, char **argv) {
 
@@ -17,31 +17,29 @@ int main(int argc, char **argv) {
 			sleep(10);
 			printf("Grandchild process with process ID %d has completed.\n",getpid());
 			exit(0);
-		}else if(gpid < 0){
+		}
+		else if(gpid < 0){
 			perror("ERROR: forking child process failed\n");
-      		exit(3);
+			exit(3);
 		}
 		
 		//Child waits for grandshild to exit, then prints its message here.
-		int returnStatus;    
-    	waitpid(gpid, &returnStatus, 0);
-    	if(returnStatus == 0){
+		int returnStatus;
+		waitpid(gpid, &returnStatus, 0);
+		if(returnStatus == 0){
 			printf("Child process with process ID %d has completed.\n",getpid());
 			exit(0);
 		}
-
 	}
 	else if(pid < 0){
 			perror("ERROR: forking child process failed\n");
-      		exit(3);
-		}
+			exit(3);
+	}
 	//Parent waits for the child to exit, and then prints its ID.
 	else{
-		int returnStatus;    
-	    waitpid(pid, &returnStatus, 0);
-
-	    if (returnStatus == 0) 
-	       printf("Parent process with process ID %d has completed.\n",getpid());    
-		}
-	
+		int returnStatus;
+		waitpid(pid, &returnStatus, 0);
+	  if (returnStatus == 0)
+	  	printf("Parent process with process ID %d has completed.\n",getpid());
+	 }
 }
--- Lab 1/part3.c
+++ Lab 1/part3.c
@@ -2,15 +2,65 @@
 #include <unistd.h> 
 #include <stdio.h> 
 #include <string.h>
-
+#include <sys/wait.h>
 
 int main(int argc, char **argv) {
+	//prints the current working directory for user clarity
+	printf("starting batch-shell\n");
+	char dir[1000];
+	getcwd(dir, sizeof(dir));
+	fprintf(stdout, "Current directory is: %s\n", dir);
+	
+	//begin main loop of program
 	while(1){
 		printf("\n");
 		printf("batch-shell> ");
 		
-		char program_string[1000];   
-		if (strcmp((fgets(program_string,sizeof program_string,stdin)), "quit\n") == 0) 
-			break;
+		//recive user input and handle the quit command
+		char inputLine[1000];
+		if (strcmp((fgets(inputLine,sizeof inputLine,stdin)), "quit\n") == 0)
+			exit(0);
+		
+		//trim the newline at the end of user input
+		char* tokens;
+		tokens = strtok(inputLine, "\n");
+		strcpy(inputLine, tokens);
+		
+		//prepare the return status and tokenize the user input by space
+		int returnStatus;
+		tokens = strtok(inputLine, " ");
+		
+		//execute each program
+		while(tokens != NULL){
+		
+			//fork the process
+			pid_t id = fork();
+			switch(id){
+				case -1: //case error
+				{
+					perror("ERROR: forking child process failed\n");
+					exit(3);
+				}
+				case 0: //case child
+				{
+					printf("\nStarting program: %s, pid: %d\n", tokens, getpid());
+					//execute the program and handle file not found
+					if(execl(tokens, tokens, NULL) == -1){
+						perror("ERROR");
+						exit(3);
+					}
+					exit(0);
+				}
+				default: //case parent
+				{
+					//wait for child to finish before executing next program and handle errors from child
+					waitpid(id, &returnStatus, 0);
+					if(WEXITSTATUS(returnStatus) == 3)
+						printf("\nProgram: %s, pid: %d encountered a nonfatal error\n", tokens, getpid());
+				}
+			}
+			//get next token
+			tokens = strtok(NULL, " ");
+		}
 	}
 }
